035d51a8dd087ee283a22e1679d78b78b5b1a4e4
pearlbob <bob@bsteele.com>
2019-03-04 09:59:50 -0800

    javadoc updates

--- src/main/java/com/lincolnRobotics/lincolnSimulation/Main.java
+++ src/main/java/com/lincolnRobotics/lincolnSimulation/Main.java
@@ -2,28 +2,16 @@ package com.lincolnRobotics.lincolnSimulation;
 
 
 import com.lincolnRobotics.robotAutonomous.RobotMotionSequencerSet;
-import com.lincolnRobotics.robotControl.RobotMotionSequencer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.Modifier;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.TreeSet;
-
 /**
  * The JavaFx application main routine.
  * JavaFx was chosen to minimize the impact on the Java code
  * in the lincolnControl package.
- * <p>To control logging from the logging.propertiesfile:  in the run pulldown, Edit Configurations,
- * Maven, sim, runner, VM options: -Djava.util.logging.config.file=logging.properties</p>
  * <p>Note: Make sure to set the JAVA_HOME environment variable to the proper JDK.
  * Java and the JDK should be either version 8 or 11.  From the command line run: java -version" to verify your JDK.
  * If it reports as a JRE, you will need to download the JDK 11 from https://jdk.java.net (11.0.2 at this time)
@@ -40,7 +28,30 @@ import java.util.TreeSet;
  * &lt;release&gt;.  Note that for version 8, the release tag is commented out.</p>
  * <p>The Java version needs to be set in File, Project Structure, Project Settings, Project, Project SDK.
  * Also the project language level should be at least 8.</p>
- * <p>To run the project outside of Intellij Idea, in the project directory run:  mvn exec:java</p>
+ * <p>To run and compile project in Idea:</p>
+ * <ul>
+ * <li>Project view (left pane)</li>
+ * <li>open lincolnJavafx</li>
+ * <li>drill down: main, java, com.lincolnRobotics, lincolnSimulation, Main</li>
+ * <li>in right pane, left click</li>
+ * <li>run "Main.main()"</li>
+ * <li>exit running application</li>
+ * <li>from run pull down select: (i.e. Main)</li>
+ * <li>select Edit Configurations</li>
+ * <li>select Application</li>
+ * <li>select Main</li>
+ * <li>Before launch:</li>
+ * <li>+</li>
+ * <li>Run Maven goal</li>
+ * <li>command line: install</li>
+ * <li>+</li>
+ * <li>Run Maven goal</li>
+ * <li>command line: javadoc:javadoc</li>
+ * <li>OK</li>
+ * </ul>
+ * Open the javadoc with a browser at: ${projectDirectory}/target/site/apidocs/index.html
+ * <p>To run the project outside of Intellij Idea, navigate in the command line to the
+ * project directory and run:  mvn exec:java</p>
  * <p>The project javaDocs can be found in ${project_directory}/target/apidocs/index.html</p>
  */
 public class Main extends Application {
--- src/main/java/com/lincolnRobotics/package-info.java
+++ src/main/java/com/lincolnRobotics/package-info.java
@@ -11,22 +11,28 @@
  * </ul>
  * <p>This demands that the dependency on these environments should not leak to
  * the robot control package.</p>
- * <p>Notes for use on Intellij Idea from Jetbrains</p>
+ * <p>Notes for use of Intellij Idea from Jetbrains</p>
  * <ul>
- * <li>java -version</li>
- * <li>https://jdk.java.net/
- * <ul><li>Java 9 or higher does not automatically include JavaFx (the graphics package for the simulation)</li>
- * <li>Java 8 does include JavaFx so https://jdk.java.net/java-se-ri/8 will be a little easier.</li>
+ * <li>Install Java 8 or Java 11 JDK
+ * <ul><li>from the command line, try:  java -version</li>
+ * <li>Either version of Java can be made to work.</li>
+ * <li>Note: You will need the JDK (development kit) not the JRE (runtime environment).  The JDK includes a JRE.</li>
+ * <li>Java 11 from https://jdk.java.net/ does not automatically
+ * include JavaFx (the graphics package for the simulation).  This can be
+ * fixed for java 11 in the pom.mxl file.  I suggest at least java 11.0.2</li>
+ * <li>Java 8 does include JavaFx so https://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
+ * can work as well.  I suggest at least jdk1.8.0_201.</li>
+ * <li>It's always going to help to have the latest update.</li>
  * </ul></li>
- * <li>https://www.jetbrains.com/idea/
+ * <li>Install https://www.jetbrains.com/idea/
  * <ul><li>Use the community version.</li>
  * </ul></li>
- * <li>-Djava.util.logging.config.file=logging.properties</li>
- * <li>compile javadoc:javadoc exec:java</li>
- * <li>Caused by: java.lang.IllegalStateException: Location is not set.
- * <ul><li></li>
- * <li>Java 8 does include JavaFx so https://jdk.java.net/java-se-ri/8 will be a little easier.</li>
- * </ul></li>
+ * <li>To control logging from the logging.properties file:  in the run pulldown, Edit Configurations,
+ * Maven, sim, runner, VM options: -Djava.util.logging.config.file=logging.properties</li>
+ * <li>See the documentation for the <a href="./lincolnSimulation/Main.html">Main class</a> for more details
+ * on project setup.</li>
+ * <li>If you get a crash with the message: "Caused by: java.lang.IllegalStateException: Location is not set.",
+ * this means the pom.xml file has not been adjusted properly for the Java version.</li>
  * </ul>
  * <p>Potential Student Projects </p>
  * <ul>
